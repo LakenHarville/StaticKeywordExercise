cf80564ab182a46b01a04f82fd0e7d4f1de295aa
Laken Harville <rlakenharville@gmail.com>
2022-04-07 01:06:09 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StaticKeywordExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(43);
+            Console.WriteLine($"Celsius = {celsius}");
+            Console.WriteLine();
+
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(6.111);
+            Console.WriteLine($"Fahrenheit = {fahrenheit}");
+        }
+    }
+}
---
+++ TempConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticKeywordExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double f)
+        {
+            return (f - 32) / 1.8; 
+        }
+
+        public static double CelsiusToFahrenheit(double c)
+        {
+            return (c * 9) / 5 + 32; 
+        }
+        
+    }
+}
